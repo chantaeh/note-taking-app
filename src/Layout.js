9848b45fae94223bab271e5748595e1617aaaa3c
chantaeh <chantae.ho@ucalgary.ca>
2023-02-21 23:35:17 -0700

    create Layout component

---
+++ src/Layout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+    return (
+        <>
+            <h1>Lotion</h1>
+            <p>Like Notion, but worse.</p>
+        </>
+    );
+}
+
+export default Layout;
