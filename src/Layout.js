20e17b63506add779383ea86fb9c19e8cb3a0569
chantaeh <chantae.ho@ucalgary.ca>
2023-02-27 14:34:14 -0700

    Added container, body-container, body-container contents

--- src/Layout.js
+++ src/Layout.js
@@ -3,21 +3,27 @@ import { Outlet } from "react-router-dom";
 function Layout() {
     return (
         <>
-            <header>
-            <span class="header-item">
-                <ul id="ul-nav">
-                    <li id="menu">&#9776;</li>
-                </ul>
-            </span>
-            <span class="header-item name">
-                <h1>Lotion</h1>
-                <p class="subname">Like Notion, but worse.</p>
-            </span>
-            <span class="header-item">
-            </span>
-        </header>
+            <div id="container">
+                <header>
+                <span class="header-item">
+                    <ul id="ul-nav">
+                        <li id="menu">&#9776;</li>
+                    </ul>
+                </span>
+                <span class="header-item name">
+                    <h1>Lotion</h1>
+                    <p class="subname">Like Notion, but worse.</p>
+                </span>
+                <span class="header-item">
+                </span>
+            </header>
             
-            
+            <div id="body-container">
+                <div id="sidebar">a</div>
+                <div id="body">b</div>
+            </div>
+        
+        </div>
         </>
     );
 }
