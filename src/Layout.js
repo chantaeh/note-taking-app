a6fade3364bbe26eda50b7895f01eb45ed7022e7
chantaeh <chantae.ho@ucalgary.ca>
2023-02-27 14:09:58 -0700

    added header

--- src/Layout.js
+++ src/Layout.js
@@ -3,8 +3,18 @@ import { Outlet } from "react-router-dom";
 function Layout() {
     return (
         <>
-            <h1>Lotion</h1>
-            <p>Like Notion, but worse.</p>
+            <header>
+            <span class="header-item">
+            </span>
+            <span class="header-item">
+                <h1>Lotion</h1>
+                <p class="subheader">Like Notion, but worse.</p>
+            </span>
+            <span class="header-item">
+            </span>
+        </header>
+            
+            
         </>
     );
 }
