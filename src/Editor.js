035d3f1a1b48d3f07ed91aa507862a4870da3168
chantaeh <chantae.ho@ucalgary.ca>
2023-03-09 17:51:37 -0700

    Change what variables are set to depending on whether an id is given or not

--- src/Editor.js
+++ src/Editor.js
@@ -1,7 +1,7 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 import {parse, stringify} from 'flatted';
 
@@ -9,19 +9,46 @@ import {parse, stringify} from 'flatted';
 function Editor() {
     const navigate = useNavigate();
     const { noteId } = useParams();
-    const [id, getNotes] = useOutletContext();
-
-    let [name, setName] = useState(parse(localStorage.getItem(id))[1]);
-    let [content, setContent] = useState(parse(localStorage.getItem(id))[3]=="..." ? "" : parse(localStorage.getItem(id))[3]);
-    const onChange = (text) => setContent(text);
+    const [id, getNotes, setTempState] = useOutletContext();
+    let tempId = id;
+    console.log(tempId);
 
     // Get current datetime and format it
     const currDateTime = new Date();
     let dateStr = currDateTime.toISOString();
     dateStr = dateStr.slice(0, 11);
     dateStr += currDateTime.toTimeString().slice(0, 8);
+    
+    let [name, setName] = useState("");
+    const [date, setDate] = useState(dateStr);
+    let [content, setContent] = useState("");
 
-    const [date, setDate] = useState(    parse(localStorage.getItem(id))[2]==" " ? dateStr : parse(localStorage.getItem(id))[2]    );
+
+    useEffect(() => {
+        if (id == "") {
+            let notesList = [];
+            Object.keys(localStorage).forEach(function(key, index) {
+                let temp = [key, ...parse(localStorage.getItem(key))];
+                notesList.push(temp);
+            });       
+            
+            // Sort entries by creation date, from newest to oldest
+            notesList.sort((a, b) => b[1].localeCompare(a[1]));
+    
+            setName(notesList[noteId-1][2]);
+            setDate(notesList[noteId-1][3]==" " ? dateStr : notesList[noteId-1][3]);
+            setContent(notesList[noteId-1][4]=="..." ? "" : notesList[noteId-1][4]);
+
+            tempId = notesList[noteId-1][0];
+        } else {
+            setName(parse(localStorage.getItem(id))[1]);
+            setDate(parse(localStorage.getItem(id))[2]==" " ? dateStr : parse(localStorage.getItem(id))[2]);
+            setContent(parse(localStorage.getItem(id))[3]=="..." ? "" : parse(localStorage.getItem(id))[3]);
+        }
+    }, [id]);
+    
+    
+    const onChange = (text) => setContent(text);
 
     const dateChanged = (event) => {
         setDate(event.target.value);
@@ -33,12 +60,12 @@ function Editor() {
 
 
     const saveNote = () => {
-        const creationDate = parse(localStorage.getItem(id))[0];
+        const creationDate = parse(localStorage.getItem(tempId))[0];
         if (content === "") {
             content = "...";
         }
         const temp = [creationDate, name, date, content]
-        localStorage.setItem(id, stringify(temp));
+        localStorage.setItem(tempId, stringify(temp));
         getNotes();
         navigate(`/notes/` + noteId);
     }
@@ -46,7 +73,7 @@ function Editor() {
     const deleteNote = () => {
         const answer = window.confirm("Are you sure?");
         if (answer) {
-            localStorage.removeItem(id);
+            localStorage.removeItem(tempId);
             navigate(`/notes`);
         }
     }
